f52e0935884e49cdd32685e450588d8534ea3ddd
yichieh_chen <yichieh_chen@pegatroncorp.com>
2014-08-06 18:10:21 +0800

    first release

---
+++ BOM_Matrix/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace BOM_Matrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.檔案FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.工具TToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.檔案FToolStripMenuItem,
+            this.工具TToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(781, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 檔案FToolStripMenuItem
+            // 
+            this.檔案FToolStripMenuItem.Name = "檔案FToolStripMenuItem";
+            this.檔案FToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.檔案FToolStripMenuItem.Text = "檔案(F)";
+            // 
+            // 工具TToolStripMenuItem
+            // 
+            this.工具TToolStripMenuItem.Name = "工具TToolStripMenuItem";
+            this.工具TToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.工具TToolStripMenuItem.Text = "工具(T)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 409);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 檔案FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 工具TToolStripMenuItem;
+    }
+}
+
---
+++ BOM_Matrix_cmd/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+using System.IO;
+using NPOI.HSSF.Util;
+using NPOI.HSSF.UserModel;
+
+/*
+透過NPOI來讀取EXCEL,要先下載NPOI DLL去參考,說明如下:
+NPOI.DLL：NPOI 核心函式庫。
+NPOI.DDF.DLL：NPOI 繪圖區讀寫函式庫。
+NPOI.HPSF.DLL：NPOI 文件摘要資訊讀寫函式庫。
+NPOI.HSSF.DLL：NPOI Excel BIFF 檔案讀寫函式庫。
+NPOI.Util.DLL：NPOI 工具函式庫。
+NPOI.POIFS.DLL：NPOI OLE 格式存取函式庫。
+ICSharpCode.SharpZipLib.DLL：檔案壓縮函式庫。
+*/
+
+namespace BOM_Matrix_cmd
+{
+    public class SheetModifyProductList
+    {
+        public string setProductNumber { get; set; }
+    }
+
+    class EXCEL
+    {
+        private string fileName = null; // Data name
+        private string Datasource = null;
+
+        public EXCEL(string fileName)
+        {
+            this.fileName = fileName;
+            this.Datasource = fileName;
+        }
+
+        public void ExcelToList()
+        {
+            IWorkbook wk;
+            ISheet st;
+            List<SheetModifyProductList> modelList = new List<SheetModifyProductList>(); //add
+
+            using (FileStream fs = new FileStream(Datasource, FileMode.Open, FileAccess.Read))
+            {
+                if (Datasource.Contains(".xlsx")) //2007
+                {
+                    wk = new XSSFWorkbook(fs);
+                    st = (XSSFSheet)wk.GetSheetAt(0);
+                }
+                else //2003
+                {
+                    wk = new HSSFWorkbook(fs);
+                    st = (HSSFSheet)wk.GetSheetAt(0);
+                }
+
+                for (int k = 0; k < wk.NumberOfSheets; k++)
+                {
+                    var hs = wk.GetSheetAt(k);
+                    var hr = hs.GetRow(0); //row
+
+                    int i = 0;
+                    hr = hs.GetRow(i); //column
+                    for (int j = hr.FirstCellNum; j <= hr.LastCellNum; j++)
+                    {
+                        if (hr.GetCell(j).ToString() != null)
+                        {
+                            Console.Write("({0},{1}) = {2} ; ", i, j, hr.GetCell(j).ToString());
+                        }
+                    }
+                }
+
+                /*
+                for (int k = 0; k < wk.NumberOfSheets; k++) //讀出sheetname
+                {
+                    var hs = wk.GetSheetAt(k); //sheet
+                    string sheetname = hs.SheetName.ToString(); //sheet's name
+                    Console.WriteLine("Sheet name: {0}.{1}", k, sheetname);
+                    if (sheetname != "N61 FF") continue;
+
+                    var hr = hs.GetRow(0); //row
+                    for (int i = hs.FirstRowNum; i <= hs.LastRowNum; i++)
+                    {
+                        hr = hs.GetRow(i); //column
+                        for (int j = hr.FirstCellNum; j < hr.LastCellNum; j++)
+                        {
+                            SheetModifyProductList model = new SheetModifyProductList(); //add
+
+                            if (i == 0 && hr.GetCell(j) != null)
+                            {
+                                //Console.Write("({0},{1}) = {2} ; ", i, j, hr.GetCell(j).ToString());
+                                model.setProductNumber = hr.GetCell(j).ToString() + ";";
+                            }
+                            modelList.Add(model);
+                        }
+                        //Console.WriteLine("\t\n");
+                    }
+                }
+                */
+
+                wk = null; //全部Sheet讀完關閉Excel
+                fs.Close();
+
+                foreach (var item in modelList)
+                {
+                    Console.Write(item.setProductNumber);
+                }
+                Console.Read();
+            }
+        }
+    }
+
+    class sample_test
+    {
+        public void test()
+        {
+            //建立Excel 2003檔案
+            IWorkbook wb = new HSSFWorkbook();
+            ISheet ws = wb.CreateSheet("Class");
+
+            ////建立Excel 2007檔案
+            //IWorkbook wb = new XSSFWorkbook();
+            //ISheet ws = wb.CreateSheet("Class");
+
+            ws.CreateRow(0);//第一行為欄位名稱
+            ws.GetRow(0).CreateCell(0).SetCellValue("name");
+            ws.GetRow(0).CreateCell(1).SetCellValue("score");
+            
+            ws.CreateRow(1);//第二行之後為資料
+            ws.GetRow(1).CreateCell(0).SetCellValue("abey");
+            ws.GetRow(1).CreateCell(1).SetCellValue(85);
+            ws.CreateRow(2);
+            ws.GetRow(2).CreateCell(0).SetCellValue("tina");
+            ws.GetRow(2).CreateCell(1).SetCellValue(82);
+            ws.CreateRow(3);
+            ws.GetRow(3).CreateCell(0).SetCellValue("boi");
+            ws.GetRow(3).CreateCell(1).SetCellValue(84);
+            ws.CreateRow(4);
+            ws.GetRow(4).CreateCell(0).SetCellValue("hebe");
+            ws.GetRow(4).CreateCell(1).SetCellValue(86);
+            ws.CreateRow(5);
+            ws.GetRow(5).CreateCell(0).SetCellValue("paul");
+            ws.GetRow(5).CreateCell(1).SetCellValue(82);
+            
+            FileStream file = new FileStream(@"c:\npoi.xls", FileMode.Create);//產生檔案
+            wb.Write(file);
+            file.Close();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "C:\\Panda.xlsx";
+            EXCEL excel = new EXCEL(file);
+            excel.ExcelToList();
+        }
+    }
+}
